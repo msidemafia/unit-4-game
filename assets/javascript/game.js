4ac0f81f9bfce71e29c954a59af787f189252b66
Chesterfield <boyce.davidw@gmail.com>
2018-12-01 19:51:45 -0500

    fourth commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,49 +7,53 @@ var totalScore = 0;
 var wins = 0;
 var losses = 0;
 
+
+
+var $imgOne = $('<img>', {
+    src: 'assets/images/crystal-one.jpg',
+    class: 'buttonImg',
+});
+var $imgTwo = $('<img>', {
+    src: 'assets/images/crystal-two.png',
+    class: 'buttonImg',
+});
+var $imgThree = $('<img>', {
+    src: 'assets/images/crystal-three.png',
+    class: 'buttonImg',
+});
+var $imgFour = $('<img>', {
+    src: 'assets/images/crystal-four.jpg',
+    class: 'buttonImg',
+});
+var buttonOne = $('<div>').append($imgOne)
+var buttonTwo = $('<div>').append($imgTwo)
+var buttonThree = $('<div>').append($imgThree)
+var buttonFour = $('<div>').append($imgFour)
+
+$('#crystalBox').append(buttonOne, buttonTwo, buttonThree, buttonFour)
+
+
 initialize();
 
 function initialize() {;
     totalScore = 0;
-    // buttonOne.attr('data-value', (Math.floor(Math.random() * 12) + 1));
-    // buttonTwo.attr('data-value', (Math.floor(Math.random() * 12) + 1));
-    // buttonThree.attr('data-value', (Math.floor(Math.random() * 12) + 1));
-    // buttonFour.attr('data-value', (Math.floor(Math.random() * 12) + 1));
+    buttonOne.attr('data-value', (Math.floor(Math.random() * 12) + 1));
+    buttonTwo.attr('data-value', (Math.floor(Math.random() * 12) + 1));
+    buttonThree.attr('data-value', (Math.floor(Math.random() * 12) + 1));
+    buttonFour.attr('data-value', (Math.floor(Math.random() * 12) + 1));
     computerChoice = Math.floor((Math.random() * 101) + 19);
     $('#totalScoreBox2').html(totalScore);
-    $("#randomNumberBox").html(computerChoice);
+    $("#randomNumberBox").html('<h3>' + computerChoice + '</h3>');
     $('#winLossBox').html("Wins: " + wins + "<br>" + "Losses: " + losses)
 
     
 }
 
-    var $imgOne = $('<img>', {
-        src: 'assets/images/crystal-one.jpg',
-        class: 'buttonImg',
-    });
-    var $imgTwo = $('<img>', {
-        src: 'assets/images/crystal-two.png',
-        class: 'buttonImg',
-    });
-    var $imgThree = $('<img>', {
-        src: 'assets/images/crystal-three.png',
-        class: 'buttonImg',
-    });
-    var $imgFour = $('<img>', {
-        src: 'assets/images/crystal-four.jpg',
-        class: 'buttonImg',
-    });
-
-    var buttonOne = $('<div>').append($imgOne).attr('data-value', (Math.floor(Math.random() * 12) + 1));
-    var buttonTwo = $('<div>').append($imgTwo).attr('data-value', (Math.floor(Math.random() * 12) + 1));
-    var buttonThree = $('<div>').append($imgThree).attr('data-value', (Math.floor(Math.random() * 12) + 1));
-    var buttonFour = $('<div>').append($imgFour).attr('data-value', (Math.floor(Math.random() * 12) + 1));
- 
-    $('#crystalBox').append(buttonOne, buttonTwo, buttonThree, buttonFour)
+    
 
     function score(buttonObject) {
         totalScore += parseInt($(buttonObject).attr('data-value'));
-        $('#totalScoreBox').html(totalScore);
+        $('#totalScoreBox2').text(totalScore);
 
         if (totalScore == computerChoice) {
             var youWin = alert ("You Win!");
